9ff6506dd6efe6c9cd87c050c1847c7ad0e26755
pedroprmartins <pauloregistecnico@gmail.com>
2025-01-17 23:03:04 -0300

    feat: Cria as váriaveis com os valores do usuário.

---
+++ app.js
@@ -0,0 +1,8 @@
+
+function sortear() {
+    let qtdNumeros = document.getElementById('quantidade').value;
+    let valorInicial = document.getElementById('de').value;
+    let valorFInal = document.getElementById('ate').value;
+
+
+}
